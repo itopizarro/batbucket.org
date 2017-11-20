e7e4904fb33d615cf1eaf7cd6e82f999891bd1bc
MVC <michael@colianna.net>
2017-11-20 16:23:45 -0500

    batbucket.org

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="/css/index.css" />
+        <title>batbucket.org</title>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-3395974929947545",
+            enable_page_level_ads: true
+          });
+        </script>
+    </head>
+
+    <body>
+        <div id="batbucket"></div>
+
+        <p>Batbucket.org © 2017 by Ito Pizarro (&amp; Michael V. Colianna, to some extent)</p>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-64910461-6"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-64910461-6');
+        </script>
+    </body>
+</html>
